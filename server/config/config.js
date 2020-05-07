50b7dd8d288777aacd1cd40300557d7df223fea4
alifdelgado <alifdelgado@gmail.com>
2020-05-07 02:00:20 -0500

    Corrigiendo archivo de configuracion

--- server/config/config.js
+++ server/config/config.js
@@ -4,6 +4,6 @@ process.env.NODE_ENV = process.env.NODE_ENV|| 'dev';
 
 let urlDB;
 
-urlDB = 'mongodb+srv://icaro:IXiaEKfx3B95TfUb@cluster0-k3x4r.mongodb.net/cafe';
+urlDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/cafe' : 'mongodb+srv://icaro:IXiaEKfx3B95TfUb@cluster0-k3x4r.mongodb.net/cafe';
 
 process.env.URLDB = urlDB;
